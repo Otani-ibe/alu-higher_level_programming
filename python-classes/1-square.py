3aa9d7d4b83139ffd7d5ef53636a820dcefa2ab5
Otani-ibe <o.ibe@alustudent.com>
2024-07-03 02:35:00 -0700

    square in size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,29 @@
 #!/usr/bin/python3
+"""
+Square Module
+
+This module defines the Square class for representing squares.
+"""
+
+
 class Square:
+    """
+    A class that defines a square.
+
+    Attributes:
+    __size (int): Private attribute representing the size of the square.
+    """
+
+
     def __init__(self, size):
+        """
+        Initializes a Square instance.
+
+        Parameters:
+        size (int): The size of the square.
+
+        Returns:
+        None
+        """
+
         self.__size = size

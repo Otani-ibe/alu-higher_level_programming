bd40278b8884cacf95ee3d6a1900fa7d2983c383
Otani-ibe <o.ibe@alustudent.com>
2024-07-03 07:39:47 -0700

    How many instances

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -103,7 +103,11 @@ class Rectangle:
         Returns:
             str: String representation of the rectangle.
         """
-        return "\n".join(["#" * self.__width for _ in range(self.__height)]) if self.__width > 0 and self.__height > 0 else ""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        rows = ["#" * self.__width for _ in range(self.__height)]
+        return "\n".join(rows)
 
     def __repr__(self):
         """

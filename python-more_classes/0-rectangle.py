4c42d138e74e40547082f0ff42a87953de86cb63
Otani-ibe <o.ibe@alustudent.com>
2024-07-03 04:24:58 -0700

    simple rectangle

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
+"""
+Rectangle Module
+
+This module defines the Rectangle class for representing rectangles.
+"""
+
+
 class Rectangle:
+    """
+    A class that defines a rectangle.
+
+    Attributes:
+    No attributes defined yet.
+    """
+
     pass

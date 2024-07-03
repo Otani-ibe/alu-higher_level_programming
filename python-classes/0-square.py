dfc01d8532005744dabe2d552c7bb0ff3ad2ac3c
Otani-ibe <o.ibe@alustudent.com>
2024-07-03 02:00:20 -0700

    my first square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 class Square:
-    """
-    Represents a square with a dictionary attribute.
-
-    Attributes:
-    dict_ (dict): A dictionary attribute.
-    """
 
     def __init__(self):
-        self.dict_ = {}
+        pass

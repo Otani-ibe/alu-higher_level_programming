5198f9c7cb4991e78529135b9cf662088ed0e3d7
Otani-ibe <o.ibe@alustudent.com>
2024-07-13 08:41:09 -0700

    only sub class of

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,20 @@
+#1/usr/bin/python3
+"""
+Module: inheritance_check
+
+This module provides a function to check if an obj is an instance of a class
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Checks if the object is an instance of a class that inherited from the specified class.
+
+    Args:
+    - obj: Any Python object.
+    - a_class: The class to check against.
+
+    Returns:
+    - bool: True if obj is an instance of a class that inherited from a_class
+    """
+    return issubclass(type(obj), a_class) and type(obj) is not a_class

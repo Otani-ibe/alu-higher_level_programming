3510b517565ebaa651ae9f9c9b3d77e112476593
Otani-ibe <o.ibe@alustudent.com>
2024-07-13 09:40:20 -0700

    rectangle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,60 +1,18 @@
 #!/usr/bin/python3
-"""
-Module: rectangle
-
-This module defines the Rectangle class that inherits from BaseGeometry.
-
-Classes:
-- Rectangle: A class representing a rectangle, inherits from BaseGeometry.
-"""
+""" Base Geometry Class inherited"""
 
 
-class BaseGeometry:
-    """
-    A class with methods for calculating area and validating integers.
-    """
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
-    def area(self):
-        """
-        Raises an exception indicating that the area method is not implemented
-        """
-        raise Exception("area() is not implemented")
 
-    def integer_validator(self, name, value):
-        """
-        Validates that value is an integer greater than 0.
+""" Program that creates a Rectangle and intance its values """
 
-        Args:
-        - name: A string representing the name of the parameter.
-        - value: The value to validate.
-
-        Raises:
-        - TypeError: If value is not an integer.
-        - ValueError: If value is less than or equal to 0.
-        """
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
 
 class Rectangle(BaseGeometry):
-    """
-    A class representing a rectangle, inherits from BaseGeometry.
-
-    Attributes:
-    - width: The width of the rectangle (private).
-    - height: The height of the rectangle (private).
-    """
-
+    """ class Rectangle that inherits from BaseGeometry """
     def __init__(self, width, height):
-        """
-        Initializes a Rectangle with width and height.
-
-        Args:
-        - width: The width of the rectangle.
-        - height: The height of the rectangle.
-        """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        """ Constructor """
         self.__width = width
         self.__height = height
+        BaseGeometry.integer_validator(self, "width", self.__width)
+        BaseGeometry.integer_validator(self, "height", self.__height)

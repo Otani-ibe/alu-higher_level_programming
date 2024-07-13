f27ae0d23c29007be1edb993cc2caad3e13fef4d
Otani-ibe <o.ibe@alustudent.com>
2024-07-13 08:47:43 -0700

    Geometry module

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module: base_geometry
+
+This module defines an empty class BaseGeometry.
+
+Author: [Your Name]
+
+Classes:
+- BaseGeometry: An empty class.
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class.
+    """
+    pass

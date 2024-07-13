a584d9f88759de4e10ccdee3701ce7b64646ec9f
Otani-ibe <o.ibe@alustudent.com>
2024-07-13 05:09:38 -0700

    lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,12 +1,45 @@
 #!/usr/bin/python3
+"""
+This is a sample module that demonstrates module, class, and function.
+
+Classes:
+- MyClass: A sample class demonstrating attributes and methods.
+  
+Functions:
+- lookup(obj): Returns a list of available attributes and methods of an object.
+"""
+
+class MyClass:
+    """
+    This is a sample class.
+
+    Attributes:
+    - data: A sample attribute storing some data.
+
+    Methods:
+    - method(): A sample method returning a string.
+    """
+
+    def __init__(self):
+        self.data = "example data"
+    
+    def method(self):
+        """
+        A sample method returning a string.
+
+        Returns:
+        - str: A fixed string indicating method completion.
+        """
+        return "example method"
+
 def lookup(obj):
     """
-    Returns a list of attributes and methods of an object.
+    Returns a list of available attributes and methods of an object.
 
     Args:
-    obj (object): The object to inspect.
+    - obj: Any Python object
 
     Returns:
-    list: List of strings representing attributes and methods of the object.
+    - List of strings: Names of attributes and methods of the object
     """
-    return dir(obj)
+    return dir (obj)

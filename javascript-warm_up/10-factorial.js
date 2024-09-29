d7334813fac7368e03e15750d7e6657ab1169f60
Otani-ibe <o.ibe@alustudent.com>
2024-09-29 09:08:09 -0700

    factorials

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,12 +1,17 @@
 #!/usr/bin/node
-const args = process.argv.slice(2).map(Number);
-
-if (args.length <= 1) {
-    console.log(0);
+function factorial(n) {
+    if (isNaN(n)) {
+        return 1;  // Return 1 for NaN
+    }
+    if (n <= 1) {
+        return 1;  // Base case: factorial(0) and factorial(1) are 1
+    }
+    return n * factorial(n - 1);  // Recursive case
 }
 
-else {
-    const sortedArgs = args.sort((a, b) => b - a);
-    console.log(sortedArgs[1]);
-}
+const input = process.argv[2];  // First argument from command line
+const num = parseInt(input, 10);  // Cast the argument to an integer
+
+console.log(factorial(num));
+
 
